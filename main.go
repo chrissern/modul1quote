bf1778f7a760b8a6be3932f7ebc9d85d6e2f4d2e
Christoffer <chriskr2002@gmail.com>
2022-01-28 14:58:24 +0100

    finally :D

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/chrissern/modul1quote/quotes"
 )
 
 func main() {
-	fmt.Println()
+	fmt.Println(quotes.GetGo())
+	fmt.Println(quotes.GetGlass())
+	fmt.Println(quotes.GetOpt())
+	fmt.Println(quotes.GetHello())
 }
